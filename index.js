03e01d0698f6cd79b5f8f15fafe241aa28cd5290
edgarpf <edgarpeixoto14@gmail.com>
2018-07-06 18:16:03 -0300

    404 treatment

--- index.js
+++ index.js
@@ -5,64 +5,85 @@ const wait = require('wait-for-stuff');
 const URL_SITE_QUOTE_PT_BR = 'https://www.pensador.com/';
 const URL_SITE_ENGLISH_QUOTE = 'https://www.brainyquote.com/authors/'
 
-var isComplete = {'isReadComplete' : false};
+var isComplete = {
+    'isReadComplete': false
+};
 var sentences = [];
 
-module.exports = (name, language = 'en', numberOfPages = 1) => {	
-	sentences = [];
-	name = name.toLowerCase().replace(' ', '_');
-	
-	if(language === 'en'){
-		readEnglishQuotes(name);
-	}else if(language === 'pt_br'){
-		readBrazilianPortugueseQuotes(name, numberOfPages);
+module.exports = (name, language = 'en', numberOfPages = 1) => {
+    sentences = [];
+    name = name.toLowerCase().replace(' ', '_');
+
+    if (language === 'en') {
+        readEnglishQuotes(name);
+    } else if (language === 'pt_br') {
+        readBrazilianPortugueseQuotes(name, numberOfPages);
+    } else{
+		isComplete.isReadComplete = true;
 	}
-	
-	wait.for.value(isComplete, 'isReadComplete' ,true);
-	isComplete.isReadComplete = false;
-	return sentences;
+
+    wait.for.value(isComplete, 'isReadComplete', true);
+    isComplete.isReadComplete = false;
+    return sentences;
 }
 
-function readEnglishQuotes(name){
-	scraperjs.StaticScraper.create(URL_SITE_ENGLISH_QUOTE+name)
-		.scrape(function($) {
-			return $(".b-qt").map(function() {
-				 sentences.push($(this).text());
-				 isComplete.isReadComplete = true;
-			}).get();
-		})
-}
-
-function readBrazilianPortugueseQuotes(name, numberOfPages){
-	scraperjs.StaticScraper.create(URL_SITE_QUOTE_PT_BR + name)
-			.scrape(($) => {
-				$(".autorTotal strong, .total").map(function() {				
-					var text = $(this).text();
-					var condition = 'de {quantity}';
-					var numberOfSentencesPerPage = 20;
-									
-					var numberOfQuotes = parseInt(extractValues(text, condition).quantity); 				
-					var numberMaxOfPages = parseInt(numberOfQuotes/numberOfSentencesPerPage);	
-
-					if(numberOfPages > numberMaxOfPages || numberOfPages === 'max'){
-						numberOfPages = numberMaxOfPages;
-					}
-					
-					if(numberOfQuotes % numberOfSentencesPerPage !== 0 && numberOfPages > 1){
-						numberOfPages++;
-					}
-													
-					$(".frase").map(function(){sentences.push($(this).text());});	
-					
-					isComplete.isReadComplete = numberOfPages === 1;
-					                 			
-					for(var i=2;i<=numberOfPages;i++){
-						scraperjs.StaticScraper.create(URL_SITE_QUOTE_PT_BR  + name + '/' + i)
-						.scrape(function ($) {
-							$(".frase").map(function(){sentences.push($(this).text());});
-							isComplete.isReadComplete = sentences.length > (numberOfPages-1)*numberOfSentencesPerPage;																								
-						});
-					}	
-				});
+function readEnglishQuotes(name) {
+    scraperjs.StaticScraper.create(URL_SITE_ENGLISH_QUOTE + name)
+        .scrape(function($) {
+			$(".thought-bubble").map(function() 
+			{					
+				isComplete.isReadComplete = true;                				
 			});
+			
+            return $(".b-qt").map(function() {
+                sentences.push($(this).text());
+                isComplete.isReadComplete = true;
+            }).get();
+        })
+}
+
+function readBrazilianPortugueseQuotes(name, numberOfPages) {
+    scraperjs.StaticScraper.create(URL_SITE_QUOTE_PT_BR + name)
+        .scrape(($) => {
+			
+			$(".top h1").map(function() 
+			{
+					if($(this).text().includes('404')){
+						isComplete.isReadComplete = true;
+					}
+			});
+			
+            $(".autorTotal strong, .total").map(function() {
+                var text = $(this).text();				
+                var condition = 'de {quantity}';
+                var numberOfSentencesPerPage = 20;
+
+                var numberOfQuotes = parseInt(extractValues(text, condition).quantity);
+                var numberMaxOfPages = parseInt(numberOfQuotes / numberOfSentencesPerPage);
+
+                if (numberOfPages > numberMaxOfPages || numberOfPages === 'max') {
+                    numberOfPages = numberMaxOfPages;
+                }
+
+                if (numberOfQuotes % numberOfSentencesPerPage !== 0 && numberOfPages > 1) {
+                    numberOfPages++;
+                }
+
+                $(".frase").map(function() {
+                    sentences.push($(this).text());
+                });
+
+                isComplete.isReadComplete = numberOfPages === 1;
+
+                for (var i = 2; i <= numberOfPages; i++) {
+                    scraperjs.StaticScraper.create(URL_SITE_QUOTE_PT_BR + name + '/' + i)
+                        .scrape(function($) {
+                            $(".frase").map(function() {
+                                sentences.push($(this).text());
+                            });
+                            isComplete.isReadComplete = sentences.length > (numberOfPages - 1) * numberOfSentencesPerPage;
+                        });
+                }
+            });
+        });
 }
--- test.js
+++ test.js
@@ -6,3 +6,8 @@ quoteScraper('mark twain', 'pt_br');
 quoteScraper('mark twain', 'pt_br', 5); 
 quoteScraper('mark twain', 'pt_br', 1000); 
 quoteScraper('mark twain', 'pt_br', 'max'); 
+quoteScraper('mark twain', 'unknow language', 'max'); 
+
+//page not found
+quoteScraper('mdscscsdcain222', 'pt_br', 5);
+quoteScraper('georgfdbfgbfgbfgbg');
